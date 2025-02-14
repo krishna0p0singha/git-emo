f7fc9899d72672bf7e2899db189e67a273606c24
cyber0bravo <cyber0bravo@gmail.com>
2025-02-14 16:37:48 +0530

    Project Initiated

---
+++ src/main/java/com/example/git_emo/GitEmoApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_emo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitEmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_emo/GitEmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_emo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitEmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
