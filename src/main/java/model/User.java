2ff5c16dbffddc2bdd38783edb15729676c4a39f
cyber0bravo <cyber0bravo@gmail.com>
2025-02-15 16:04:56 +0530

    Adding User Entity

---
+++ src/main/java/model/User.java
@@ -0,0 +1,31 @@
+package model;
+
+public class User {
+    private int userId;
+    private String name;
+    private int password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+}
