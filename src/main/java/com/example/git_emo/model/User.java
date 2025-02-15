45a14b38379a37c09887409dc7b34ceb3c9fa8b4
cyber0bravo <cyber0bravo@gmail.com>
2025-02-15 16:10:07 +0530

    Modiying user -> adding field email

--- src/main/java/com/example/git_emo/model/User.java
+++ src/main/java/com/example/git_emo/model/User.java
@@ -4,15 +4,6 @@ public class User {
     private int userId;
     private String name;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    private String email;
     private int password;
 
     public int getUserId() {
